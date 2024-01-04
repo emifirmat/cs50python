9de5795566f773726d4bca9f847998ce839de056
Emiliano Caiciia <emifirmat_cp@hotmail.com>
2024-01-03 23:30:18 -0300

    Startin project

---
+++ project.py
@@ -0,0 +1,29 @@
+def main():
+    # Welcome menu...
+        # Welcome
+        # Ask name
+        # Say hello
+        # Menu options: Start game, Rules, exit
+
+
+    # Rules
+        # Deck
+        # Envido
+        # Truco
+        # Scores
+
+
+    # Jugability
+        # Dealer
+        # First row
+        # Second row
+        # Third row
+    
+    
+    # Score
+        # Sum total score
+        # Check total score
+
+
+if __name__ == "__main__":
+    main()    
