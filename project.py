e53efaf17c884461171b2b86e156c022876683e2
Emiliano Caiciia <emifirmat_cp@hotmail.com>
2024-01-06 18:29:17 -0300

    Rules and Exit functions done

--- project.py
+++ project.py
@@ -4,33 +4,77 @@ from time import sleep
 from pyfiglet import Figlet
 from classes import Menu
 
+
 def main():
     # Welcome
-    introduction()
+    ...
     
     # Menu options: Start game, Rules, exit
-    menu = Menu()
-    print(menu, end="\n\n")
-    
-    # User answer
-    choise = get_answer()
+    while True:
+        menu = Menu([
+            ["start", "Start game"],
+            ["rules", "Learn how to play"],
+            ["quit", "Exit game"],
+        ])
+        print(menu, end="\n\n")
+        
+        # User answer
+        choise = menu.get_answer()
 
-    # Move to options
-    match choise:
-        case "start":
-            play()
-        case "rules":
-            rules()
-        case "quit":
-            exit("Thank you for playing truco, see you!")
+        # Move to options
+        match choise:
+            case "start":
+                play()
+            case "rules":
+                rules()
+            case "quit":
+                exit_game()
+
     
 # Rules
 def rules():   
-    ...
-    # Deck
-    # Envido
-    # Truco
-    # Scores
+    while True:
+    
+        # Intro rules
+        print("--To learn how to play, choose a command from the menu", end="\n\n")
+            
+        # Rules menu
+        menu = Menu([
+            ["intro"],
+            ["cards"],
+            ["main"],
+            ["envido"],
+            ["truco"],
+            ["scores"],
+        ])
+
+        # Print menu and choose
+        print(menu, end="\n\n")
+        rchoise = menu.get_answer()
+        sleep(1)
+        
+        if rchoise == "quit" or rchoise =="exit":
+            print()
+            sleep(1)
+            return 
+        
+        # Show text from file
+        try:
+            with open(f"rules/{rchoise}.txt") as file:
+                print(file.read(), end="\n\n")
+        except FileNotFoundError:
+            print("Sorry, command is not working, try another option")
+        
+        # Ask to continue or leave rules
+        sleep(1)
+        fchoise = input("Type <any letter> to continue navigating or <quit> to go back.\n\n")
+        sleep(1)
+
+        if fchoise == "quit":
+            print()
+            sleep(1)
+            return 
+
 
 def play():
     ...
@@ -52,24 +96,29 @@ def introduction():
     print(figlet.renderText("Truco card game"))
     sleep(1)
 
-    # Ask name
-    name = input("What's your name?\n\n").strip()
+    # Ask name and limit length up to 20 chars
+    name = input("--What's your name?\n\n").strip()
     name = name[0:21]
     print()
     sleep(1)
 
     # Say hello
-    print(f"Welcome {name}! Read the menu in order to start your game:", end="\n\n")    
+    print(f"--Welcome {name}! Read the menu in order to start your game:", end="\n\n")    
     sleep(2)    
 
 
-def get_answer():
-    """
-    Let de user choise
-    """  
-    while choise := input("Command:").strip().lower() not in ["start", "rules", "quit"]:
-        continue
-    return choise
+def exit_game():
+    print()
+    print("Exit game? (Yes/No)")
+    menu = Menu([
+        ["yes"],
+        ["no"],
+    ])
+
+    answer = menu.get_answer()
+
+    if answer == "yes":
+        exit("--Thank you for playing truco, see you!")
 
 
 if __name__ == "__main__":
